b62dd16617f189e7ac62f6daaf89cbd777308046
Adrian21 <stroescuadrian@gmail.com>
2024-04-24 11:25:00 +0300

    more js

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 'use strict';
 
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
-/*document.querySelector('.number').textContent=secretNumber;*/
+let score = 20;
+// document.querySelector('.number').textContent = secretNumber;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -9,13 +10,33 @@ document.querySelector('.check').addEventListener('click', function () {
 
   if (!guess) {
     document.querySelector('.message').textContent = '⛔ No Number!';
-  } else if (guess > secretNumber) {
-    document.querySelector('.message').textContent = '📈 Too high!';
-  } else if (guess < secretNumber) {
-    document.querySelector('.message').textContent = '📉 Too low!';
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = '🍕 Correct Number!';
     document.querySelector('.number').textContent = secretNumber;
+    document.querySelector('.highscore').textContent = score;
+    document.querySelector('.number').style.width = '30rem';
     document.querySelector('body').style.backgroundColor = '#60b347';
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📈 Too high!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '💀 You lost the game!';
+      document.querySelector('.score').textContent = 0;
+      document.querySelector('body').style.backgroundColor = '#963535';
+      document.querySelector('.number').textContent = '💀';
+    }
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📉 Too low!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '💀 You lost the game!';
+      document.querySelector('.score').textContent = 0;
+      document.querySelector('body').style.backgroundColor = '#963535';
+      document.querySelector('.number').textContent = '💀';
+    }
   }
 });
